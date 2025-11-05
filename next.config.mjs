fe3345a19c5c95fe1abe555d049cef9b98e5a90e
Benito Pedro <benitopedro21@outlook.com>
2025-11-05 18:39:31 -0300

    wip: added blog posts page + search

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/media/**',
+      },
     ],
     qualities: [75, 90, 95, 100],
   },
