fd2902d9c0142da079e507205712f29354ee8b13
Benito Pedro <benitopedro21@outlook.com>
2025-11-22 20:03:44 -0300

    fix: update hostname in HTTPS configuration from aibe.website to www.aibe.website for correct domain handling

--- next.config.mjs
+++ next.config.mjs
@@ -29,7 +29,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'aibe.website',
+        hostname: 'www.aibe.website',
         port: '',
         pathname: '/**',
       },
